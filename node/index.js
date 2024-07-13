2753000d58cf22cd89baf33ec9804e075a2eb4c5
guilhermesilveiraa <guilherme.silveira@icomp.ufam.edu.br>
2024-07-13 19:27:08 -0400

    node1 part2

--- node/index.js
+++ node/index.js
@@ -1,31 +1,34 @@
-const fs = require('fs')
-const http = require('http')
+const fs = require('fs');
+const http = require('http');
+
+require('dotenv').config();
 
 const args = [];
 const arquivos = [];
 
-process.argv.forEach((val, index) => {
-    args.push(val)
-})
-const path = `${__dirname}${args[2]}`
+process.argv.forEach((val,index) =>{
+    args.push(val);
+});
 
-const filesLs = fs.readdir(path, async (err, files) => {
-    if (err) 
-        console.log(err); 
-      else { 
-    files.forEach(file => {
-        arquivos.push(file)
-    })}
-})    
+const path = `${__dirname}${args[2]}`;
 
-const server = http.createServer((req, res) => {
+const filesLs = fs.readdir(path, async(err, files) =>{
+    if(err)
+        console.log(err);
+    else{
+        files.forEach(file=>{
+            arquivos.push(file);
+        })
+    }
+});
+
+const server = http.createServer((req,res)=>{
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end(arquivos.reduce((acc, curr) =>  acc + String(curr) + '\n', ''));
-  });
+    res.setHeader("Content-type", "text/plain");
+    res.end(arquivos.reduce((acc,curr) => acc + String(curr) + '\n', ''));
+});
 
-
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running`);
-  });
+    console.log('Servidor rodando');
+});
