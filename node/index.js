496c6b8c38577fe3b1c60775c9d53669ab4b50ac
guilhermesilveiraa <guilherme.silveira@icomp.ufam.edu.br>
2024-07-14 11:34:59 -0400

    node1 parte 6

--- node/index.js
+++ node/index.js
@@ -1,34 +1,52 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('path');
 
 require('dotenv').config();
+const { createLink } = require('./str_helper');
 
 const args = [];
 const arquivos = [];
 
-process.argv.forEach((val,index) =>{
+process.argv.forEach((val, index) => {
     args.push(val);
 });
 
-const path = `${__dirname}${args[2]}`;
+const dirPath = path.join(__dirname, args[2]);
 
-const filesLs = fs.readdir(path, async(err, files) =>{
-    if(err)
+fs.readdir(dirPath, (err, files) => {
+    if (err) {
         console.log(err);
-    else{
-        files.forEach(file=>{
+    } else {
+        files.forEach(file => {
             arquivos.push(file);
-        })
+        });
     }
 });
 
-const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader("Content-type", "text/plain");
-    res.end(arquivos.reduce((acc,curr) => acc + String(curr) + '\n', ''));
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/html");
+        const links = arquivos.reduce((acc, curr) => acc + createLink(curr), '');
+        res.end(links);
+    } else {
+        const filePath = path.join(dirPath, req.url);
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                res.statusCode = 404;
+                res.setHeader("Content-type", "text/plain");
+                res.end("File not found");
+            } else {
+                res.statusCode = 200;
+                res.setHeader("Content-type", "text/html");
+                res.end(data + '<br> <a href="/">Voltar</a>');
+            }
+        });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log('Servidor rodando');
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
