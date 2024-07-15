fe8f27b6b06dccf4f973611abcf32dcfdf1f0268
guilhermesilveiraa <guilherme.silveira@icomp.ufam.edu.br>
2024-07-15 10:08:01 -0400

    Exercicio 3 de Express conc

--- ExpTS/src/index.ts
+++ ExpTS/src/index.ts
@@ -1,12 +1,22 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
+import logger from "./middlewares/logger";
+
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT ?? 3333;
+
+const logFormat = process.env.LOG_FORMAT === 'complete' ? 'complete' : 'simple';
+
+app.use(logger(logFormat));
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world!");
+    res.send("Olá Mundo");
+});
+
+app.get("/about", (req: Request, res: Response) => {
+    res.send("Página about");
 });
 
 app.listen(PORT, () => {
